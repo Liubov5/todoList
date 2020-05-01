7f5390641b6ba079a81e701d5cb272f0ce36772d
liubov5 <liubov.iakovleva5@gmail.com>
2020-05-01 19:45:14 +0900

    lol

--- app/Console/Commands/TestLiuba.php
+++ app/Console/Commands/TestLiuba.php
@@ -44,7 +44,7 @@ class TestLiuba extends Command
         // DB::table('todos')->where("status", 0)->delete();
          //dd(Carbon::today()->format('l'));
 
-         if(Carbon::now()->setTimezone('GMT+9')->format('H:i' ) == "21:15" ) {
+         if(Carbon::now()->setTimezone('GMT+9')->format('H:i' ) == "19:45" ) {
 
           $deals = DB::table('todos')->where("status", 0)->where('date', Carbon::today()->format('Y-m-d'))->get();
 
